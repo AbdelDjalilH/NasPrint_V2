aff195173faac528cd4c57b6bb5459996694487d
AbdelDjalilH <adj.hamzaoui@gmail.com>
2024-09-29 23:04:27 +0200

    debut init serverjs

---
+++ backend/server.js
@@ -0,0 +1,3 @@
+// Ce fichier sert à générer un serveur express qui écoute sur un port donné
+// Ce fichier définit des routes d'accès pour mon API (endponts)
+// Ce fichier ouvre une connexion vers une base de donnée Mysql
